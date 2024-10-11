a432813b19751067e01a11cbcc122b1f668084fa
liucharlie213 <liu.charlie213@gmail.com>
2024-10-11 19:47:02 -0400

    began lexer

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# COMS4115
-Programming Assignments for COMS4115 PLT
+# COMS4115-HW1
+
+## Lexical Grammar
+1. keywords: if | else | when | start | end
+2. strings: [$][a-z]+[$]
+3. integers: [0-9]+
+4. operators: + | - | * | \ | = 
+5. identifer: [@][a-z]+[0-9]?
+
+1. keywords: variable | control[WHITESPACE]flow | operation | target[whitespace]language | style | if | else | for | while | do | done | break | continue | and
+2. identifers:  
+3. operators: add | multiply | subtract | divide | equals | equal[ws]to | greater[]than | less[]than | >= | <= | != | not
+- not equals to vs not equals to
+
+
+
+variables will define all the identifiers
+control flow will define all the control flow statements
+
+for i in range(10):
+print(i)
+
+for (int i = 0; i < 10; i++) {
+  print(i)
+}
---
+++ lexer.py
@@ -0,0 +1,95 @@
+class Token:
+  def __init__(self, type, value):
+    self.type = type
+    self.value = value
+  
+  def __repr__(self):
+    return f'<{self.type}, {self.value}>'
+
+class Lexer:
+  def __init__(self, input_string):
+    self.input = input_string
+    self.pointer = 0
+    self.tokens = []
+
+  def scan(self):
+    # print("IM SCANNING")
+    print('input string: ', self.input)
+    while self.pointer < len(self.input):
+      char = self.input[self.pointer]
+
+      if char in ['{', '}', '[', ']', ':', ',']:
+        self.handle_single_character(char)
+      elif char.isdigit() or char == '-':
+        self.handle_number()
+      elif char == '"':
+
+        self.handle_string()
+      elif char.isalpha():
+        self.handle_keyword()
+      elif char.isspace():
+        self.pointer += 1
+      else:
+        # explore this more
+        self.handle_error()
+
+    return self.tokens
+  
+  def handle_single_character(self, char):
+    token_type = {
+      '{': 'LBRACE',
+      '}': 'RBRACE',
+      '[': 'LBRACK',
+      ']': 'RBRACK',
+      ':': 'COLON',
+      ',': 'COMMA',
+    }[char]
+
+    self.tokens.append(Token(token_type, char))
+    self.pointer += 1
+  
+  def handle_number(self):
+    start = self.pointer
+    while self.pointer < len(self.input):
+      
+      if self.input[self.pointer].isdigit() or self.input[self.pointer] == '-':
+        self.pointer += 1
+      # pointer is pointer one index beyond the last digit
+      number = self.input[start:self.pointer]
+      self.tokens.append(Token('NUMBER', number))
+  
+  def handle_string(self):
+    # starts at '"'
+    print("handling string")
+    start = self.pointer
+    self.pointer += 1
+    string_contents = []
+    while self.pointer < len(self.input) and self.input[self.pointer] != '"':
+      if self.input[self.pointer] == '\\' or self.input[self.pointer] == '\r' or self.input[self.pointer] == '\n':
+        # upon invalid character, just ignore it
+        self.pointer += 1
+
+      else:
+        # upon valid character, append
+        string_contents.append(self.input[self.pointer])
+        self.pointer += 1
+
+    print("string: ", ''.join(string_contents))
+    self.pointer += 1 # skip the closing pointer
+
+    self.tokens.append(Token('STRING', ''.join(string_contents)))
+  
+  def handle_keyword(self):
+    print("skipping keyword for now")
+    pass
+
+
+
+def main():
+  print("Hello, World!")
+  lexer = Lexer('{"name": "John", "age": 30, "city": "Montana"}')
+  tokens = lexer.scan()
+  print(tokens)      
+
+main()
+  
